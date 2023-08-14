937e7a61cd2a9c83a3c167597a7905995b4bc10d
Anish Ranjan <137291124+a85nishranjan@users.noreply.github.com>
2023-08-15 04:53:36 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,42 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+//int amc(int a ,int b );
+int main() {
+     
+   int a=10;
+    int b=30 ;
+    int c;
+  c=  amc(a,b);
+    cout <<c;
+
+    return 0;
+}
+
+
+int amc(int a, int b){
+    int sum= a+b;
+    return sum;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+
+
+
+
